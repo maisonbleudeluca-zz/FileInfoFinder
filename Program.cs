bacd6df5366b370adec02c6b74d29269cf78dfb6
Luca <Luca@DESKTOP-3CD82ES>
2021-12-23 15:15:14 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press E to exit. ");
+
+
+            while (CalculateAnswer()) ;
+
+
+        }
+
+        static bool CalculateAnswer()
+        {
+            Console.WriteLine("Please input a valid file path: \n");
+
+            string filepath = Console.ReadLine();
+
+            if (filepath.ToLower().Trim() == "e")
+            {
+                return false;
+            }
+
+            while (!File.Exists(filepath))
+            {
+                Console.WriteLine("Please input a valid file path.");
+                filepath = Console.ReadLine();
+            }
+
+            FileInfo file = new FileInfo(filepath);
+
+            Console.Write($"\nCreated at: {file.CreationTimeUtc} UTC\n");
+            Console.Write($"Last modified at: {file.LastWriteTimeUtc} UTC\n");
+            Console.Write($"File size: {file.Length} bytes\n");
+            Console.Write($"File extension: {file.Extension}\n");
+
+            return true;
+        }
+    }
+}
